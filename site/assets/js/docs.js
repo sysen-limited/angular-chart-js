39009956fa3e4efa76ad663239416a74117b3cc0
Andrew Cunliffe <andrew.c.cunliffe@gmail.com>
2015-05-23 18:42:26 +0100

    Tidy up documentation to match for graphs

--- site/assets/js/docs.js
+++ site/assets/js/docs.js
@@ -57,12 +57,12 @@ angular.module('sysen', ['ngMaterial', 'sysChart'])
             labels: ["January", "February", "March", "April", "May", "June", "July"],
             datasets: [
                 {
-                    fillColor: "rgba(220,220,220,0.2)",
+                    fillColor: "rgba(220,220,220,0.4)",
                     strokeColor: "rgba(220,220,220,1)",
                     data: [65, 59, 80, 81, 56, 55, 40]
                 },
                 {
-                    fillColor: "rgba(151,187,205,0.2)",
+                    fillColor: "rgba(151,187,205,0.4)",
                     strokeColor: "rgba(151,187,205,1)",
                     data: [28, 48, 40, 19, 86, 27, 90]
                 }
